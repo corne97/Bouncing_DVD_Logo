1b644ac9b2ad207beb58d62a04ff6c776bddce9d
corne97 <c0rn31997@hotmail.com>
2023-10-30 14:39:40 +0100

    small bug fixes

--- index.js
+++ index.js
@@ -1,71 +1,66 @@
-let speed = 12;
-let scale = 0.17;
+let speed = 35;
+let scale = 0.17; 
 let canvas;
 let ctx;
 let logoColor;
 
 let dvd = {
-	x: 200,
-	y: 300,
-	xspeed: 10,
-	yspeed: 10,
-	img: new Image()
+    x: 200,
+    y: 300,
+    xspeed: 10,
+    yspeed: 10,
+    img: new Image()
 };
 
-(function main()
-{
-	canvas = document.getElementById("DVDLogo");
-	ctx = canvas.getContext("2d");
-	dvd.img.src = 'dvd-logo.png';
+(function main(){
+    canvas = document.getElementById("DVDLogo");
+    ctx = canvas.getContext("2d");
+    dvd.img.src = 'dvd-logo.png';
 
-	canvas.width = windwow.innerWidth;
-	canvas.heigt = window.innerHeight;
+    canvas.width  = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-	chooseColor();
-	update();
+    chooseColor();
+    update();
 })();
 
-function update()
-{
-	setTimeout(() =>
-	{
-		// Draw canvas background
-		ctx.fillStyle = '#000';
-		ctxfillRect(0, 0, canvas.width, canvas.heigt);
-		// set DVD logo and background
-		ctx.fillStyle = logoColor;
-		ctx.fillRect(dvd.x, dvd.y, dvd.img, width * scale, dvd.img.height * scale);
-		ctx.drawImage(dvd.img, dvd.x, dvd.y, dvd.img.width * scale, dvd.img.height * scale);
+function update() {
+    setTimeout(() => {
+        //Draw the canvas background
+        ctx.fillStyle = '#000';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        //Draw DVD Logo and his background
+        ctx.fillStyle = logoColor;
+        ctx.fillRect(dvd.x, dvd.y, dvd.img.width*scale, dvd.img.height*scale);
+        ctx.drawImage(dvd.img, dvd.x, dvd.y, dvd.img.width*scale, dvd.img.height*scale);
 
-		// logo movement
-		dvd.x += x.speed;
-		dvd.y += y.speed;
-
-		hitBox();
-		update();
-	}, speed)
+        // DVD logo movement
+        dvd.x+=dvd.xspeed;
+        dvd.y+=dvd.yspeed;
+        //Check for collision 
+        checkColission();
+        update();   
+    }, speed)
 }
 
-// border collision check
-function hitBox()
-{
-	if (dvd.x + dvd.img.width * scale >= canvas.width || dvd.x <= 0)
-	{
-		dvd.xspeed *= -1;
-		chooseColor();
-	}
-	if (dvd.y + dvd.img.height * scale >= canvas.height || dvd.y <= 0)
-	{
-		dvd.yspeed *= -1;
-		chooseColor();
-	}
+// collision check
+function checkColission(){
+    if(dvd.x+dvd.img.width*scale >= canvas.width || dvd.x <= 0){
+        dvd.xspeed *= -1;
+        chooseColor();
+    }
+        
+    if(dvd.y+dvd.img.height*scale >= canvas.height || dvd.y <= 0){
+        dvd.yspeed *= -1;
+        chooseColor();
+    }    
 }
-// randomize colors for logo
-function chooseColor()
-{
-	r = Math.random() * (254 - 0) + 0;
-	g = Math.random() * (254 - 0) + 0;
-	b = Math.random() * (254 - 0) + 0;
 
-	logoColor = 'rgb(' + r + ',' + g + ', ' + b + ')';
+// randomize color in rgb
+function chooseColor(){
+    r = Math.random() * (254 - 0) + 0;
+    g = Math.random() * (254 - 0) + 0;
+    b = Math.random() * (254 - 0) + 0;
+
+    logoColor = 'rgb('+r+','+g+', '+b+')';
 }
